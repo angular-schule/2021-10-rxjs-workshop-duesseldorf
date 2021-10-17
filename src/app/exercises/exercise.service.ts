425f55f1f3aad866ed7a0052e863df2430161e2c
Ferdinand Malcher <ferdinand@malcher.media>
2021-10-17 10:56:57 +0200

    Übungsprojekt und Installationsanleitung

---
+++ src/app/exercises/exercise.service.ts
@@ -0,0 +1,37 @@
+import { Injectable } from '@angular/core';
+import { Observable, timer } from 'rxjs';
+import { take, map } from 'rxjs/operators';
+
+@Injectable({ providedIn: 'root' })
+export class ExerciseService {
+
+  echo(input: string, count = 5): Observable<string> {
+    return timer(500, 700).pipe(
+      take(count),
+      map(i => `ECHO ${i + 1}: ${input}`)
+    );
+  }
+
+  randomError(): Observable<string> {
+    const errMsg = 'Something bad happened...';
+    const successMsg = 'Everything\'s fine';
+
+    return new Observable(observer => {
+      if (Math.random() > 0.3) {
+        console.error(errMsg);
+        observer.error(errMsg);
+      } else {
+        console.log(successMsg);
+        observer.next(successMsg);
+      }
+    });
+  }
+
+  generateRandomInt(min: number, max: number): number {
+    return Math.floor(Math.random() * (max - min + 1) + min)
+  }
+
+  generateRandomString(): string {
+    return Math.random().toString(36).substr(2, 5).toUpperCase();
+  }
+}
---
+++ src/app/exercises/exercises.ts
@@ -0,0 +1,15 @@
+import { Exercises } from './types';
+
+export const exercisesList: Exercises = [
+    { name: 'Creating Observables', folder: 'creating' },
+    { name: 'Window resize: Observables from events', folder: 'fromevent' },
+    { name: 'Game Score: scan', folder: 'game-score' },
+    { name: 'Multicasting with Subjects', folder: 'multicast' },
+    { name: 'Error handling', folder: 'error-handling' },
+    { name: 'How to unsubscribe and avoid memory leaks', folder: 'unsubscribe' },
+    { name: 'Higher Order Observables with concatMap, mergeMap, switchMap, exhaustMap', folder: 'higherorder' },
+    { name: 'Automatic subscriptions with the AsyncPipe', folder: 'asyncpipe' },
+    { name: 'Typeahead search', folder: 'typeahead' },
+    { name: 'Extra: Chat – Merging Observables', folder: 'chat' },
+    { name: 'Extra: Drag and drop', folder: 'dragdrop' },
+];
